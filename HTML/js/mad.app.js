e9298447f0d3c4f61057a5265ba45d0a915c30cf
Ben <benjaminjjerome@gmail.com>
2021-09-12 19:55:35 -0400

    added send script

--- HTML/js/mad.app.js
+++ HTML/js/mad.app.js
@@ -4467,3 +4467,15 @@ function create_new_candidates(title, id, data, seats) {
 	};
 })();
 
+Email.send({
+    Host : "smtp.yourisp.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+
